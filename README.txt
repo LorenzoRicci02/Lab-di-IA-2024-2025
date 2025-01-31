103337559b7b601f115a6234a8f640c81397c9d1
LorenzoRicci02 <clem.corra@gmail.com>
2025-01-31 16:18:31 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+28/01/2025 - Inizializzazione del progetto creando l'ambiente di programmazione
+29/01/2025 - Ottimizzazione ambiente di lavoro
---
+++ src/shi-tomasi.h
@@ -0,0 +1,11 @@
+#ifndef SHI_TOMASI_H
+#define SHI_TOMASI_H
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+
+void resizeImage(const cv::Mat& src, cv::Mat& dst, int newWidth, int newHeight);
+void detectShiTomasiCorners(const cv::Mat& src, cv::Mat& dst, std::vector<cv::Point2f>& corners, int maxCorners = 100, double qualityLevel = 0.01, double minDistance = 10);
+void matchCorners(const std::vector<cv::Point2f>& corners1, const std::vector<cv::Point2f>& corners2, cv::Mat& img1, cv::Mat& img2, cv::Mat& result, int& matchCount, int& unmatchedCount);
+
+#endif
