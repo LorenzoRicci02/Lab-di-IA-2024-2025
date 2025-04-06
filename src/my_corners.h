5f494d542724b5fe9498b3589c08eae075d24299
LorenzoRicci02 <clem.corra@gmail.com>
2025-04-06 18:02:54 +0200

    Add files via upload

--- src/my_corners.h
+++ src/my_corners.h
@@ -9,15 +9,14 @@ using namespace std;
 
 // Shi-Tomasi
 pair<Mat, Mat> computeDerivatives(const Mat& image);
-Matx22d buildStructureMatrix(int posX, int posY, const Mat& gradX, const Mat& gradY, int kernelSize);
-vector<double> extractEigenvalues(const Matx22d& structMatrix);
+inline double smallestEigenvalue(double a, double b, double c);
 Mat nonMaximumSuppression(const Mat& responseMap, int windowSize);
 vector<KeyPoint> ShiTomasiCorners(const Mat& sourceImage, double qualityRatio, int maxPoints, int patchSize);
 
 // FAST
 bool fastSegmentTest(const Mat &img, int x, int y, int threshold = 50, int n = 8);
 bool fastHighSpeedTest(const Mat &img, int x, int y, int threshold);
-void nonMaximumSuppression(vector<KeyPoint> &keypoints, const Mat &img, int dist = 3);
+void nonMaximumSuppression(vector<KeyPoint>& keypoints, int minDist);
 vector<KeyPoint> FASTCorners(const Mat& sourceImage, int threshold, bool nonMaxSuppression);
 
 // Harris
--- src/my_orb.h
+++ src/my_orb.h
@@ -8,5 +8,7 @@ using namespace cv;
 using namespace std;
 
 void computeORB(const Mat& image1, const Mat& image2, vector<KeyPoint>& keypoints1, vector<KeyPoint>& keypoints2, Mat& descriptors1, Mat& descriptors2, vector<DMatch>& goodMatches, int fastThreshold = 50, int maxKeypoints = 500, int briefBits = 256);
+float computeOrientation(const Mat& image, const KeyPoint& kp, int patchSize);
+vector<DMatch> filterMatchesRANSAC(const vector<DMatch>& matches, const vector<KeyPoint>& kp1, const vector<KeyPoint>& kp2, double ransacThresh);
 
 #endif
